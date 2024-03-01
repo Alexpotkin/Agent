cb79471d300ef045889242e642405bdbc904df21
Alexpotkin <Alexkkm78@gmail.com>
2024-03-01 17:02:18 +0300

    Добавил вывод проверки на обновление в принудительный лог

--- components/update.ps1
+++ components/update.ps1
@@ -2,16 +2,16 @@
 function global:Update {
     # function updater
     param($ini, $ver)
-    $repoUrl = "https://github.com/Alexpotkin/agent"         # URL  GitHub
+    $repoUrl = "https://github.com/Alexpotkin/Agent"         # URL  GitHub
     try {
         $latestVersion = [System.Net.WebRequest]::Create($repoUrl + "/releases/latest").GetResponse().ResponseUri.AbsoluteUri.Split("/")[-1]
         if (($latestVersion -ne $ver) -and ("" -ne $latestVersion)) {
-            Debuging -param_debug $debug -debugmessage ("New version available. LOADING... - ($latestVersion)") -typemessage info
+            Debuging -param_debug $debug -debugmessage ("New version available. LOADING... - ($latestVersion)") -typemessage info -anyway_log $true
             # load file
             $zipUrl = $repoUrl + "/archive/$latestVersion.zip"
             $zipPath = ".\update.zip"
             Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
-            Debuging -param_debug $debug -debugmessage ("Loading file!: -  ($zipUrl)") -typemessage info 
+            Debuging -param_debug $debug -debugmessage ("Loading file!: -  ($zipUrl)") -typemessage info -anyway_log $true
 
             # unzip
             Expand-Archive -Path $zipPath -DestinationPath ".\temp" -Force
@@ -23,7 +23,7 @@ function global:Update {
             return $updateResult = 1
         }
         elseif ($latestVersion -eq $ver) {
-            Debuging -param_debug $debug -debugmessage ("Script version is current - ($ver)") -typemessage info 
+            Debuging -param_debug $debug -debugmessage ("Script version is current - ($ver)") -typemessage info -anyway_log $true
             return $updateResult = 0
         }
     }
